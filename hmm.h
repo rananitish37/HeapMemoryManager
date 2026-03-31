7a46904dc3ca1d6c9db01a8ed99550ad194fc1b1
Nitish Rana <rananitish37@gmail.com>
2026-03-31 03:09:36 +0000

    Defining data structure for the family page size

---
+++ hmm.h
@@ -0,0 +1,14 @@
+typedef struct vm_page_family_{
+	char struct_name[MM_MAX_STRUCT_NAME];
+	uint32_t struct_size;
+}vm_page_family_t;
+
+typedef struct vm_page_for_families_{
+
+	struct vm_page_for_families_ *next;
+	vm_page_family_t vm_page_family[0];
+}typedef struct vm_page_families_t;
+
+#define MAX_FAMILIES_PER_VM_PAGE \
+	(SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t *)/\
+	 sizeof(vm_page_family_t))
